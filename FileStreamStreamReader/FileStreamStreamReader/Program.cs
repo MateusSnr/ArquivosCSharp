b2eb0c530ff6fa3606a6e5cd5b45e2c3d06a98d2
“Mateus <devmateussnr@gmail.com>
2023-06-09 11:19:21 -0300

    Push inicial

---
+++ FileStreamStreamReader/FileStreamStreamReader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FileStreamStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"F:\ProjetosCSharp\file10.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                /*Mostrar apenas uma linha
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+                */
+
+                //Mostrando o arquivo todo
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro !");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) sr.Close();
+                if(fs != null) fs.Close();
+            }
+        }
+    }
+}
