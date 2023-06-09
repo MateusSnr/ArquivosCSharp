1cbe62fa4041e7cff7c9ec6342984b8f4dce3e10
“Mateus <devmateussnr@gmail.com>
2023-06-09 12:34:44 -0300

    Push inicial StreamWriter

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace streamwriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"F:\ProjetosCSharp\file10.txt";
+            string targetpath = @"F:\ProjetosCSharp\file12.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))//Concatenar no final
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro !");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
